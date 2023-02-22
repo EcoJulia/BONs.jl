dd3ae3f9a402ddeb09933629c4cd7a555b0b5142
michael <michael@pop-os.localdomain>
2023-02-22 09:54:52 -0500

    :sparkle: SpeciesDistributionTookit integration

--- src/integrations/simplesdms.jl
+++ src/integrations/simplesdms.jl
@@ -1,6 +1,5 @@
 @info "Loading BONs.jl support for SimpleSDMLayers.jl ..."
 
-
 function BiodiversityObservationNetworks.stack(layers::Vector{<:SimpleSDMLayers.SimpleSDMLayer})
     # assert all layers are the same size if we add this function to BONs.jl
     mat = zeros(size(first(layers))..., length(layers))
